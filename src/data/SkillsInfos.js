4f40d3b259f534b2e4bbd77e9bcf9f0b4094691e
caiomiyaji <c.hidekimiyaji@gmail.com>
2023-03-13 15:58:54 -0300

    Added new skills

--- src/data/SkillsInfos.js
+++ src/data/SkillsInfos.js
@@ -2,9 +2,13 @@ import html from '../assets/html-icon.svg'
 import css from '../assets/css-icon.svg'
 import js from '../assets/javascript-icon.svg'
 import reactJs from '../assets/react-icon.svg'
+import redux from '../assets/redux-icon.svg'
+import nodejs from '../assets/nodejs-icon.svg'
+import mongodb from '../assets/mongodb-icon.svg'
 import git from '../assets/git-icon.svg'
 import gitHub from '../assets/github-icon.svg'
 import bubble from '../assets/bubble-icon.svg'
+
 
 const SkillsInfos = [
     {
@@ -22,6 +26,18 @@ const SkillsInfos = [
     {
         name: "ReactJS",
         image: reactJs
+    },
+    {
+        name: "Redux",
+        image: redux
+    },
+    {
+        name: "NodeJs",
+        image: nodejs
+    },
+    {
+        name: "MongoDB",
+        image: mongodb
     },
     {
         name: "Git",
